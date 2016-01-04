03cc57efc0e7a438fb80b77e6d41cac8561a6162
vigo.feng <ilfw@qq.com>
2016-01-04 18:39:38 +0800

    update

--- src/js/components/progressbar.js
+++ src/js/components/progressbar.js
@@ -1,7 +1,43 @@
-;(function($) {
+;
+(function($) {
     var self = this;
 
+    var tpl = '<div class="progress">\
+		  <div class="progress-bar" role="progressbar">\
+		    <span class="sr-only">40% Complete (success)</span>\
+		  </div>\
+		</div>';
 
 
-    $.fn.progressbar.default = {};
+    function Progressbar(element, options) {
+        var self = this;
+        this.elem = element;
+        this.config = $.extend(true, {}, $.fn.page.defaults, options);
+        this.init(options);
+    };
+
+    Progressbar.prototype = {
+    	constructor: 'Progressbar',
+    	/**
+    	 * 初始化
+    	 */
+    	init: function () {
+    		bindEvent: function(){
+
+    		}
+    	},
+    	/**
+    	 * 绑定事件
+    	 */
+    	bindEvent: function() {
+    		
+    	}
+
+
+    }
+
+    $.fn.progressbar.default = {
+        css: '',
+
+    };
 })(jQuery)
