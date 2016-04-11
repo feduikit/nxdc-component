6738ef828c3c5557fa12022e98c42895d3d820f2
yirong.zd <yirong.zd@alibaba-inc.com>
2016-04-11 16:03:41 +0800

    修改bug 见update.log

--- src/js/config.js
+++ src/js/config.js
@@ -1,5 +1,5 @@
 require.config({
-    // urlArgs:"rnd="+(new Date()).getTime(),
+     urlArgs:"rnd="+(new Date()).getTime(),
     paths:{
         'jquery':'../../lib/jquery/jquery.min',
         'bootstrap':'../../lib/bootstrap/dist/js/bootstrap.min',
