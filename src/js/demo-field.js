8c535337acf2a4630a9ba463586b888e2a519fb6
yirong.zd <yirong.zd@alibaba-inc.com>
2016-05-12 10:27:59 +0800

    更新字体

--- src/js/demo-field.js
+++ src/js/demo-field.js
@@ -3,16 +3,6 @@ require(['./config'],function(){
         require(['bootstrap','field'],function(){
             
             var hwd=$("div.ndp-field-wrapper").field();
-            
-            
-            function cmdInHtml(){
-                var tag = "&nbsp<span class='deco-tag' readonly='true' >hello world</span>&nbsp";
-                document.execCommand("insertHTML",false,tag);
-            }
-
-            function cmdInTxt(){
-                document.execCommand("insertText",false,"");
-            } 
 
             $("#intag").click(function(){
                 hwd.addTag({val:"1130",text:'hello world'});
